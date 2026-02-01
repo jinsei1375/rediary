ace7e2a259e7d762f5f7b110542a578b4f60bc2a
Shinsei <murakami.shinsei.zn@gmail.com>
2026-02-01 17:44:10 +0900

    アップ

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ module.exports = ({ config }) => {
     expo: {
       name: 'rediary',
       slug: 'rediary',
-      version: '0.1.5',
+      version: '0.1.6',
       orientation: 'portrait',
       icon: './assets/images/icon.png',
       scheme: 'rediary',
